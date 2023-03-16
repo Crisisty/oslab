c4e5f959b7de0ec82d1e4458a8c2ed1cab474a8e
Crisisty <1154917679@qq.com>
2023-03-16 19:08:09 +0800

    add super.c

--- linux-0.12/fs/super.c
+++ linux-0.12/fs/super.c
@@ -10,13 +10,13 @@
 /*
  * super.c 程序中含有处理超级块表的代码。
  */
-#include <linux/config.h>
-#include <linux/sched.h>
-#include <linux/kernel.h>
-#include <asm/system.h>
+#include <linux/config.h>		/* 内核配置头文件。定义键盘语言和硬盘类型（HD_TYPE）选项 */
+#include <linux/sched.h>		/* 调度程序头文件。定义了任务结构task_struct、任务0的数据，还有一些有关描述符参数设置和获取的嵌入式汇编函数宏语句 */
+#include <linux/kernel.h>		/* 内核头文件。含有一些内核常用函数的原型定义 */
+#include <asm/system.h>			/* 系统头文件。定义了设置或修改描述符/中断门等的嵌入式汇编宏 */
 
-#include <errno.h>
-#include <sys/stat.h>
+#include <errno.h>				/* 错误号头文件。包含系统中各种出错号。 */
+#include <sys/stat.h>			/* 文件状态头文件。含有文件或文件系统状态结构stat{}和常量*/
 
 int sync_dev(int dev);			/* 同步高速缓冲到设备上 */
 void wait_for_keypress(void);	/* 等待击键 */
@@ -48,34 +48,42 @@ int ROOT_DEV = 0;	/* 根文件系统设备号 */
 
 /**
  * 锁定超级块
+ * 如果超级块已被锁定，则将当前任务置为不可中断的等待状态，并添加到该超级块等待队列s_wait中，
+ * 直到该超级块解锁并明确地唤醒本任务。然后对其上锁。
+ * 这三个函数（lock_super()、free_super()和wait_on_super()）的作用与inode.c程序文件中
+ * 头3个函数的作用雷同，只是这里操作的对象换成了超级块
  * @param[in]	sb		超级块指针
  * @retval		void
  */
 static void lock_super(struct super_block *sb)
 {
-	cli();
-	while (sb->s_lock) {
+	cli();							/* 关中断 */
+	while (sb->s_lock) {			/* 如果该超级块已经上锁，则睡眠等待 */
 		sleep_on(&(sb->s_wait));
 	}
-	sb->s_lock = 1;
-	sti();
+	sb->s_lock = 1;					/* 给该超级块加锁（置锁定标志） */
+	sti();							/* 开中断 */
 }
 
 /**
  * 解锁指定超级块
+ * 复位超级块的锁定标志，并明确地唤醒等待在此超级块等待队列s_wait上所有进程
+ * 如果使用ulock_super这个名称则可能更妥帖
  * @param[in]	sb		超级块指针
  * @retval		void
  */
 static void free_super(struct super_block * sb)
 {
 	cli();
-	sb->s_lock = 0;
-	wake_up(&(sb->s_wait));
+	sb->s_lock = 0;					/* 复位锁定标志 */
+	wake_up(&(sb->s_wait));			/* 唤醒等待该超级块的进程 */
 	sti();
 }
 
 /**
  * 睡眠等待超级块解锁
+ * 如果超级块已被锁定，则将当前任务置为不可中断的等待状态，并添加到该超级块的等地队列
+ * s_wait中。直到该超级块解锁并明确地唤醒本任务
  * @param[in]	sb		超级块指针
  * @retval		void
  */
@@ -83,7 +91,7 @@ static void wait_on_super(struct super_block * sb)
 {
 	cli();
 	while (sb->s_lock) {
-		sleep_on(&(sb->s_wait));
+		sleep_on(&(sb->s_wait));	/* 如果超级块已经上锁，则睡眠等待 */
 	}
 	sti();
 }
@@ -96,14 +104,24 @@ static void wait_on_super(struct super_block * sb)
  */
 struct super_block * get_super(int dev)
 {
-	struct super_block *s;
+	struct super_block *s;			/* s是超级块数据结构指针 */
+	/*
+	 * 首先判断参数所给出设备的有效性。若设备号为0则返回空指针。然后让s指向超级块数组起始处，
+	 * 开始搜索整个超级块数组，以寻找指定设备dev的超级块。s = 0 + super_block 等同于
+	 * s = super_block、s = &super_block[0]
+	 */
 	/* 若设备号为0，则返回空指针 */
 	if (!dev) {
 		return NULL;
 	}
 	s = 0 + super_block;
 	while (s < NR_SUPER + super_block) {
-		
+		/*
+		 * 如果当前搜索项是指定设备的超级块，即该超级块的设备号字段值与函数参数指定的相同，
+		 * 则先等待该超级块解锁（若已被其他进程上锁的话）。在等待期间，该超级块项有可能被
+		 * 其他设备使用，因此等待返回之后需要再判断一次是否是指定设备的超级块。如果是则返回
+		 * 该超级块的指针。否则就重新对超级块再搜索一遍，因此此时s需要又指向超级块数组开始处
+		 */
 		if (s->s_dev == dev) {
 			wait_on_super(s);
 			/* 在等待期间，该超级块项有可能被其他设备使用，因此等待返回后需再判断一次 */
@@ -112,6 +130,7 @@ struct super_block * get_super(int dev)
 			}
 			s = 0 + super_block;
 		} else {
+			/* 如果当前搜索项不是，则检查下一项。如果没有找到指定的超级块，则返回空指针 */
 			s++;
 		}
 	}
@@ -130,6 +149,12 @@ void put_super(int dev)
 {
 	struct super_block * sb;
 	int i;
+	/*
+	 * 首先判断参数的有效性和合法性。如果指定设备是根文件系统设备，则显示警告信息“根系统盘改变，准备生死决战吧”
+	 * 并返回。然后在超级块表中寻找指定设备号的文件系统超级块。如果该超级块指明该文件系统所安装到的i节点还没有
+	 * 被处理过，则显示警告信息并返回。在文件系统卸载（umount）操作中，s_imount会先被置成null以后才会调用
+	 * 本函数
+	 */
 	/* 根文件系统设备的超级块不能被释放 */
 	if (dev == ROOT_DEV) {
 		printk("root diskette changed: prepare for armageddon\n\r");
@@ -143,6 +168,13 @@ void put_super(int dev)
 		printk("Mounted disk changed - tssk, tssk\n\r");
 		return;
 	}
+	/*
+	 * 然后找到指定设备的超级块之后，我们先锁定该超级块，再置该超级块对应的设备号字段s_dev为0，也即释放该设备上的
+	 * 文件系统超级块。然后释放该超级块占用的其他内核资源，即释放该设备上文件系统i节点位图和逻辑块位图在缓冲区中所占
+	 * 用的缓冲块。下面常数符号I_MAP_SLOTS和Z_MAP_SLOTS均等于8，用于分别指明i节点位图和逻辑块位图占用的磁盘逻辑
+	 * 块数。注意，若这些缓冲块内容被修改过，则需要同步操作才能把缓冲块中的数据写入设备中。函数最后对该超级块解锁
+	 * 并返回
+	 */
 	lock_super(sb);
 	sb->s_dev = 0;	/* 置超级块空闲 */
 	/* 释放该设备上文件系统i节点位图和逻辑位图在缓冲区中所占用的缓冲块 */
@@ -169,12 +201,20 @@ static struct super_block * read_super(int dev)
 	struct buffer_head * bh;
 	int i, block;
 
+	/* 
+	 * 首先判断参数的有效性，然后检查该设备是否已更换过盘片（也即是否是软盘设备）。如果更换过盘。
+	 * 则高速缓冲区有关该设备的所有缓冲块均失效，需要进行失效处理，即释放原来加载的文件系统
+	 */
 	if (!dev) {
 		return NULL;
 	}
 	/* 检查软盘是否更换 */
 	check_disk_change(dev);
 
+	/* 
+	 * 如果设备的超级块已经在超级块表中，则直接返回该超级块的指针。否则就在超级块数组中找出一个空闲
+	 * 项（也即字段s_dev=0的项）。如果数组已经占满则返回空指针
+	 */
 	if ((s = get_super(dev))) {
 		return s;
 	}
@@ -187,12 +227,22 @@ static struct super_block * read_super(int dev)
 			break;
 		}
 	}
+	/*
+	 * 在超级块数组中找到空闲项之后，就将该超级块项用于指定设备dev上的文件系统。于是对该超级块结构中字段
+	 * 进行部分初始化处理
+	 */
 	s->s_dev = dev;		/* 用于dev设备上的文件系统 */
 	s->s_isup = NULL;
 	s->s_imount = NULL;
 	s->s_time = 0;
 	s->s_rd_only = 0;
 	s->s_dirt = 0;
+	/*
+	 * 然后锁定该超级块，并从设备上读取超级块信息到bh指向的缓冲块中。超级块位于设备的第2个逻辑块（1号块）中，
+	 * （第1个是引导盘块）。如果读超级块操作失败，则释放上面选定的超级块数组中的项（即置s_dev=0），并解锁
+	 * 该项，返回空指针退出。否则就将读取的超级块信息从缓冲块数据区复制到超级块数组相应项结构中，并释放存放
+	 * 读取信息的高级缓冲块
+	 */
 	/* 从设备上读取超级块信息到bh指向的缓冲块中，再从缓冲块复制到超级块数组中 */
 	lock_super(s);
 	if (!(bh = bread(dev, 1))) {
@@ -203,11 +253,21 @@ static struct super_block * read_super(int dev)
 	*((struct d_super_block *) s) = *((struct d_super_block *) bh->b_data);
 	brelse(bh);
 
+	/*
+	 * 现在我们从设备dev上得到了文件系统的超级块，于是开始检查该超级块的有效性，并从设备上读取i节点位图和逻辑
+	 * 块位图等信息。如果所读取的超级块的文件系统魔数字段不对，说明设备上不是正确的文件系统，因此同上面一样，释放
+	 * 上面选定的超级块数组中的项，并解锁该项，返回空指针退出
+	 */
 	if (s->s_magic != SUPER_MAGIC) { /* linux0.12只支持MINIX文件系统1.0，魔数为0x137f */
 		s->s_dev = 0;
 		free_super(s);
 		return NULL;
 	}
+	/*
+	 * 下面开始读取设备上i节点位图和逻辑块位图数据。首先初始化内存超级块结构中位图空间。然后从设备上读取i节点位图和
+	 * 逻辑块位图信息，并存放在超级块对应字段中。i节点位图保存在设备上2号块开始的逻辑块中，共占用s_imap_blocks个块。
+	 * 逻辑位图在i节点位图所在块的后续块中，共占用s_zmap_blocks个块
+	 */
 	/* 读取设备上i节点位图和逻辑块位图数据 */
 	for (i = 0; i < I_MAP_SLOTS; i++) {		/* 初始化i节点位图和逻辑块位图 */
 		s->s_imap[i] = NULL;
@@ -232,15 +292,18 @@ static struct super_block * read_super(int dev)
 	}
 	/* 如果读出的位图个数不等于位图应该占有的逻辑块数，说明文件系统位图信息有问题，超级块初始
 	 化失败，则释放所有资源 */
+	/*
+	 * 即释放i节点位图和逻辑块位图占用的高速缓冲块、释放上面选定的超级块数组项、解锁该超级块项，并返回空指针退出
+	 */
 	if (block != 2 + s->s_imap_blocks + s->s_zmap_blocks) {
-		for(i = 0; i < I_MAP_SLOTS; i++) {
+		for(i = 0; i < I_MAP_SLOTS; i++) {		/* 释放位图占用的高速缓冲块 */
 			brelse(s->s_imap[i]);
 		}
 		for(i = 0; i < Z_MAP_SLOTS; i++) {
 			brelse(s->s_zmap[i]);
 		}
 		s->s_dev = 0;		/* 释放选定的超级块数组项 */
-		free_super(s);
+		free_super(s);		/* 解锁该超级块项 */
 		return NULL;
 	}
 	/* 0号i节点和0号逻辑块不可用 */
